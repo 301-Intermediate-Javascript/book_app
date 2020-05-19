d8cedbb718b12f9a71a9d14cdb2c29334dd1b2b3
BryantDavis1986 <bryant.davis@hotmail.com>
2020-05-18 17:51:17 -0700

    final push for submission

--- server.js
+++ server.js
@@ -24,6 +24,17 @@ app.set('view engine', 'ejs');
 
 app.get('/hello', helloCallBack);
 app.get('/searches/new', searchCallback);
+app.post('/searches', searchesCallBack)
+// Constructor
+// Book
+
+function Book(object) {
+    this.title = object.title ? object.title : 'Title unknown';
+    this.author = object.author ? object.author : 'Author unknown';
+    this.image = object.image ? object.image : 'https://i.imgur.com/J5LVHEL.jpg';
+    this.description = object.description ? object.description : 'No description available';
+}
+
 // Server Callbacks
 
 function helloCallBack(request, respond){
@@ -32,6 +43,14 @@ function helloCallBack(request, respond){
 
 function searchCallback(request, respond){
     respond.render('Pages/searches/new');
+}
+
+function searchesCallBack(request, respond){
+console.log(request.body);
+const bookApiUrl = `https://www.googleapis.com/books/v1/volumes?q=${request.body.name}`
+    superAgent.get(bookApiUrl)
+    .then()
+    .catch()
 }
 //Listen
 app.listen(PORT, () => {
